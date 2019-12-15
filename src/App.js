351bb0b61bb509cc02528f46b04d9e3f69ab25a3
David Todorov <t_david@outlook.com>
2019-12-15 22:43:40 +0200

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './shared/styles.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Navigation from './Navigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path="/login">
+            <div>Login</div>
+          </Route>
+        </Switch>
+        <header className="App-header">
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactt
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Reactt
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Router>
+
   );
 }
 
