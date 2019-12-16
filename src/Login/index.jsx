2dcddff675750c2b35b915ed0e9eba7b6798e4fa
David Todorov <t_david@outlook.com>
2019-12-16 08:13:31 +0200

    removed remember me

--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -95,10 +95,6 @@ export default function SignInSide() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
